5825f6843bda971a5e082488198f8f6826e5a5ed
Sanath Kumar U <sanath.usk@gmail.com>
2024-09-01 17:30:08 +0530

    Updated the description

--- background.js
+++ background.js
@@ -1,4 +1,8 @@
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (
+  message,
+  _sender,
+  _sendResponse
+) {
   if (message.url) {
     chrome.tabs.create({ url: message.url });
   }
